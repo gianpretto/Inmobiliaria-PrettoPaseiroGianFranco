8cd27331f896e203a56b07a938b95add0adf0286
Gian Franco Pretto Paseiro <86487907+gianpretto@users.noreply.github.com>
2024-04-26 00:17:44 -0300

    Updatefinal InterfazInmobiliaria.java

--- src/ar/edu/unlam/pb1/InterfazInmobiliaria.java
+++ src/ar/edu/unlam/pb1/InterfazInmobiliaria.java
@@ -1,12 +1,13 @@
-package ar.edu.unlam.pb1;
+package ar.edu.unlam.pb2;
 
 import java.util.Scanner;
 
 public class InterfazInmobiliaria {
+	private static Scanner teclado = new Scanner(System.in);
 	private static  Inmobiliaria actual = new Inmobiliaria("PrettoPaseiro", "Callefalsa 123", "inmobiliariapretto@gmail.com","1512345678");
 
             public static void main(String[] args) {
-                Scanner teclado = new Scanner(System.in);
+                
 
                 OpcionDeMenu opcionMenu;
                 
@@ -80,7 +81,7 @@ public class InterfazInmobiliaria {
 
 	private static void agregarPropiedad(Inmobiliaria inmobiliaria){
 		
-		 Scanner teclado = new Scanner(System.in);
+
 		 
 		 
 		 System.out.println("Agregar nueva propiedad:");
@@ -90,52 +91,56 @@ public class InterfazInmobiliaria {
 		    String tipoPropiedad;
 		    do {
 		        System.out.print("Ingrese el tipo de propiedad (CASA, DEPTO, PH, TERRENO, CAMPO): \n");
-		        tipoPropiedad = teclado.nextLine().toUpperCase();
+		        tipoPropiedad = teclado.next();
+		        tipoPropiedad = tipoPropiedad.toUpperCase();
 		        if (tipoPropiedad == null) {
 		            System.out.println("Tipo de propiedad no válido. Por favor, ingrese un tipo de propiedad válido.");
 		        }
 		    } while (tipoPropiedad == null);
 
 		    		    
-		   if (tipoPropiedad == "CASA") {
+		   if (tipoPropiedad.equals("CASA") ) {
 		    	
 		    	System.out.print("Ingrese un codigo para la propiedad: ");
-	    		String codigo = teclado.nextLine(); 
+	    		String codigo = teclado.next(); 
 		    	
 		    	System.out.print("Ingrese la calle de la propiedad: ");
-		    	String calle = teclado.nextLine(); 
+		    	String calle = teclado.next(); 
 
 		    	System.out.print("Ingrese el numero de la propiedad: ");
 		    	Integer numero = teclado.nextInt();
-		    	teclado.nextLine(); 
+		    	
 		    
 		    	System.out.print("Ingrese los metros cuadrados totales de la propiedad: ");
 		    	Integer metros = teclado.nextInt();
-		    	teclado.nextLine(); 
+		    	
 		    
 		    	System.out.print("Ingrese la cantidad de Ambientes totaltes(si es un terreno debe ser cero) de la propiedad: ");
 		    	Integer cantAmbientes = teclado.nextInt();
-		    	teclado.nextLine(); 
+		    	 
 
 		    	System.out.print("Ingrese la ciudad de la propiedad: ");
 		    	String ciudad = teclado.nextLine(); 
+		    	teclado.nextLine();
 
 		    	System.out.print("Ingrese el precio de la propiedad: ");
 		    	Double precioPropiedad = teclado.nextDouble();
-		    	teclado.nextLine(); 
+		    	teclado.nextLine();
+		    	
+		    	
 
 		    	System.out.print("Ingrese si esta disponible la propiedad: ");
 		    	Boolean disponible = teclado.nextBoolean();
-		    	teclado.nextLine(); 
+		    	teclado.nextLine();
+		    	
 		   
 		    	System.out.print("Ingrese los datos del propietario separado por coma (DNI, NOMBRE, APELLIDO): ");
 		    	String propietarioStr = teclado.nextLine();
-		    	Propietario propietario = Propietario.fromString(propietarioStr);
+		    	Propietario propietario = Propietario.fromString(propietarioStr);		    	
 		    	
-		    	teclado.nextLine(); 
 		    	
 //		    	System.out.print("Ingrese los datos del inquilino separado por coma (DNI, NOMBRE, APELLIDO): ");
-//		    	String inquilinoStr = teclado.nextLine();
+//		    	String inquilinoStr = teclado.next();
 //		    	Inquilino inquilino = Inquilino .fromString(inquilinoStr);
 //	  		    
 		    	Casa nuevaCasa = new Casa(codigo, calle, numero, metros, cantAmbientes, ciudad, precioPropiedad, disponible,propietario);		   
@@ -148,52 +153,53 @@ public class InterfazInmobiliaria {
 	      }
 		    
 		   
-		   if (tipoPropiedad == "DEPTO") {
+		   if (tipoPropiedad.equals("DEPTO") ) {
 		    	
 		    	System.out.print("Ingrese un codigo para la propiedad: ");
-	    		String codigo = teclado.nextLine(); 
+	    		String codigo = teclado.next(); 
 		    	
 		    	System.out.print("Ingrese la calle de la propiedad: ");
-		    	String calle = teclado.nextLine(); 
+		    	String calle = teclado.next(); 
 
 		    	System.out.print("Ingrese el numero de la propiedad: ");
 		    	Integer numero = teclado.nextInt();
-		    	teclado.nextLine(); 
+		    	teclado.nextLine();
+		    	
 		    
 		    	System.out.print("Ingrese los metros cuadrados totales de la propiedad: ");
 		    	Integer metros = teclado.nextInt();
-		    	teclado.nextLine(); 
+		    	 
 		    	
 		    	System.out.print("Ingrese el piso donde se ubica la propiedad: ");
 		    	Integer piso = teclado.nextInt();
-		    	teclado.nextLine(); 
+		    	
 		    	
 		    	System.out.print("Ingrese la letra del piso donde se ubica la propiedad: ");
 		    	Character letra = teclado.next().charAt(0);
 		    			    
 		    	System.out.print("Ingrese la cantidad de Ambientes totaltes(si es un terreno debe ser cero) de la propiedad: ");
 		    	Integer cantAmbientes = teclado.nextInt();
-		    	teclado.nextLine(); 
+		    	
 
 		    	System.out.print("Ingrese la ciudad de la propiedad: ");
 		    	String ciudad = teclado.nextLine(); 
 
 		    	System.out.print("Ingrese el precio de la propiedad: ");
 		    	Double precioPropiedad = teclado.nextDouble();
-		    	teclado.nextLine(); 
+		    	teclado.nextLine();
 
 		    	System.out.print("Ingrese si esta disponible la propiedad: ");
 		    	Boolean disponible = teclado.nextBoolean();
-		    	teclado.nextLine(); 
+		    	
 		   
 		    	System.out.print("Ingrese los datos del propietario separado por coma (DNI, NOMBRE, APELLIDO): ");
-		    	String propietarioStr = teclado.nextLine();
+		    	String propietarioStr = teclado.next();
 		    	Propietario propietario = Propietario.fromString(propietarioStr);
 		    	
-		    	teclado.nextLine(); 
+		    	teclado.next(); 
 		    	
 //		    	System.out.print("Ingrese los datos del inquilino separado por coma (DNI, NOMBRE, APELLIDO): ");
-//		    	String inquilinoStr = teclado.nextLine();
+//		    	String inquilinoStr = teclado.next();
 //		    	Inquilino inquilino = Inquilino .fromString(inquilinoStr);
 	  		    
 		    	Departamento nuevoDepto = new Departamento(codigo, calle, numero, metros, piso, letra, cantAmbientes, ciudad, precioPropiedad, disponible,propietario);		   
@@ -206,7 +212,7 @@ public class InterfazInmobiliaria {
 	      }
 
 
-		    teclado.close();
+		    
 		}
 	
 	
@@ -217,19 +223,20 @@ public class InterfazInmobiliaria {
 
 	private static void mostrarPropiedades(Inmobiliaria inmobiliaria) {
 		
-		Scanner teclado = new Scanner(System.in);
+		
 		  
 		    String tipoPropiedad;
 		    do {
 		        System.out.print("Ingrese el tipo de propiedad (CASA, DEPTO, PH, TERRENO, CAMPO): \n");
-		        tipoPropiedad = teclado.nextLine().toUpperCase();
+		        tipoPropiedad = teclado.next();
+		        tipoPropiedad = tipoPropiedad.toUpperCase();
 		        if (tipoPropiedad == null) {
 		            System.out.println("Tipo de propiedad no válido. Por favor, ingrese un tipo de propiedad válido.");
 		        }
 		    } while (tipoPropiedad == null);
 
 		    		    
-		   if (tipoPropiedad == "CASA") {
+		   if (tipoPropiedad.equals("CASA") ) {
 	    
 			   Casa[] casas = inmobiliaria.getCasas();
 
@@ -241,7 +248,7 @@ public class InterfazInmobiliaria {
 	    
 	     }
 		   
-		   if (tipoPropiedad == "DEPTO") {
+		   if (tipoPropiedad.equals("DEPTO") ) {
 			    
 			   Departamento[] departamentos = inmobiliaria.getDepartamentos();
 
@@ -252,26 +259,27 @@ public class InterfazInmobiliaria {
 	    	}
 	    
 	     }
-		   teclado.close();
+		   
 	}    
 
 	private static void modificarPropiedad(Inmobiliaria inmobiliaria){
-		Scanner teclado = new Scanner(System.in);
+		
 		  
 	    String tipoPropiedad;
 	    do {
 	        System.out.print("Ingrese el tipo de propiedad (CASA, DEPTO, PH, TERRENO, CAMPO): \n");
-	        tipoPropiedad = teclado.nextLine().toUpperCase();
+	        tipoPropiedad = teclado.next();
+	        tipoPropiedad =tipoPropiedad.toUpperCase();
 	        if (tipoPropiedad == null) {
 	            System.out.println("Tipo de propiedad no válido. Por favor, ingrese un tipo de propiedad válido.");
 	        }
 	    } while (tipoPropiedad == null);
 
 	    
-		   if (tipoPropiedad == "CASA") {
+		   if (tipoPropiedad.equals("CASA") ) {
 			    
 			   System.out.print("Ingrese el código de la propiedad que desea modificar: ");
-			    String codigo = teclado.nextLine(); 
+			    String codigo = teclado.next(); 
 			    
 			    Casa propiedadAModificar = null;
 			    for (Casa casas : inmobiliaria.getCasas()) {
@@ -286,40 +294,40 @@ public class InterfazInmobiliaria {
 			        System.out.println("Propiedad encontrada. Por favor ingrese los nuevos detalles:");
 
 			        System.out.print("Ingrese la calle de la propiedad: ");
-			        String calle = teclado.nextLine();
+			        String calle = teclado.next();
 			        propiedadAModificar.setCalle(calle);
 			        
 			        System.out.print("Ingrese el numero de la propiedad: ");
 				    Integer numero = teclado.nextInt();
-				    teclado.nextLine();
+				    
 				    propiedadAModificar.setNumero(numero);
 				    
 				    System.out.print("Ingrese los metros cuadrados totales de la propiedad: ");
 				    Integer metros = teclado.nextInt();
-				    teclado.nextLine(); 
+				     
 				    propiedadAModificar.setMetros(metros);
 				    
 				    System.out.print("Ingrese la cantidad de Ambientes totaltes(si es un terreno debe ser cero) de la propiedad: ");
 				    Integer cantAmbientes = teclado.nextInt();
-				    teclado.nextLine(); 
+				     
 				    propiedadAModificar.setCantAmbientes(cantAmbientes);
 
 				    System.out.print("Ingrese la ciudad de la propiedad: ");
-				    String ciudad = teclado.nextLine(); 
+				    String ciudad = teclado.next(); 
 				    propiedadAModificar.setCiudad(ciudad);
 
 				    System.out.print("Ingrese el precio de la propiedad: ");
 				    Double precioPropiedad = teclado.nextDouble();
-				    teclado.nextLine(); 
+				     
 				    propiedadAModificar.setPrecio(precioPropiedad);
 
 				    System.out.print("Ingrese si esta disponible la propiedad: ");
 				    Boolean disponible = teclado.nextBoolean();
-				    teclado.nextLine(); 
+				    
 				    propiedadAModificar.setEstaDisponible(disponible);
 				    
 				    System.out.print("Ingrese los datos del propietario separado por coma (DNI, NOMBRE, APELLIDO): ");
-			    	String propietarioStr = teclado.nextLine();
+			    	String propietarioStr = teclado.next();
 			    	Propietario propietario = Propietario.fromString(propietarioStr);
 			    	propiedadAModificar.setPropietario(propietario);
 
@@ -330,10 +338,10 @@ public class InterfazInmobiliaria {
 
 	    	}
 		   
-		   if (tipoPropiedad == "DEPTO") {
+		   if (tipoPropiedad.equals("DEPTO") ) {
 			    
 			   System.out.print("Ingrese el código de la propiedad que desea modificar: ");
-			    String codigo = teclado.nextLine(); 
+			    String codigo = teclado.next(); 
 			    
 			    Departamento propiedadAModificar = null;
 			    for (Departamento departamentos : inmobiliaria.getDepartamentos()) {
@@ -348,50 +356,50 @@ public class InterfazInmobiliaria {
 			        System.out.println("Propiedad encontrada. Por favor ingrese los nuevos detalles:");
 
 			        System.out.print("Ingrese la calle de la propiedad: ");
-			        String calle = teclado.nextLine();
+			        String calle = teclado.next();
 			        propiedadAModificar.setCalle(calle);
 			        
 			        System.out.print("Ingrese el numero de la propiedad: ");
 				    Integer numero = teclado.nextInt();
-				    teclado.nextLine();
+				    
 				    propiedadAModificar.setNumero(numero);
 				    
 				    System.out.print("Ingrese los metros cuadrados totales de la propiedad: ");
 				    Integer metros = teclado.nextInt();
-				    teclado.nextLine(); 
+				     
 				    propiedadAModificar.setMetros(metros);
 				    
 				    System.out.print("Ingrese los metros cuadrados totales de la propiedad: ");
 				    Integer piso = teclado.nextInt();
-				    teclado.nextLine(); 
+				    
 				    propiedadAModificar.setPiso(piso);
 				    
 				    System.out.print("Ingrese los metros cuadrados totales de la propiedad: ");
 				    Character letra = teclado.next().charAt(0);
-				    teclado.nextLine(); 
+				    
 				    propiedadAModificar.setLetra(letra);
 				    
 				    System.out.print("Ingrese la cantidad de Ambientes totaltes(si es un terreno debe ser cero) de la propiedad: ");
 				    Integer cantAmbientes = teclado.nextInt();
-				    teclado.nextLine(); 
+				     
 				    propiedadAModificar.setCantAmbientes(cantAmbientes);
 
 				    System.out.print("Ingrese la ciudad de la propiedad: ");
-				    String ciudad = teclado.nextLine(); 
+				    String ciudad = teclado.next(); 
 				    propiedadAModificar.setCiudad(ciudad);
 
 				    System.out.print("Ingrese el precio de la propiedad: ");
 				    Double precioPropiedad = teclado.nextDouble();
-				    teclado.nextLine(); 
+				    
 				    propiedadAModificar.setPrecio(precioPropiedad);
 
 				    System.out.print("Ingrese si esta disponible la propiedad: ");
 				    Boolean disponible = teclado.nextBoolean();
-				    teclado.nextLine(); 
+				    
 				    propiedadAModificar.setEstaDisponible(disponible);
 				    
 				    System.out.print("Ingrese los datos del propietario separado por coma (DNI, NOMBRE, APELLIDO): ");
-			    	String propietarioStr = teclado.nextLine();
+			    	String propietarioStr = teclado.next();
 			    	Propietario propietario = Propietario.fromString(propietarioStr);
 			    	propiedadAModificar.setPropietario(propietario);
 
@@ -402,17 +410,16 @@ public class InterfazInmobiliaria {
 
 	    	}
 	    
-	    teclado.close();
+	    
 	}
 	
 	private static void agregarCliente(Inmobiliaria inmobiliaria) {
-			Scanner teclado = new Scanner(System.in);
 			
 			
 		    String tipoCliente;
 		    do {
 		    	System.out.print("Ingrese el tipo de cliente(INQUILINO o PROPIETARIO): ");
-		         tipoCliente = teclado.nextLine().toUpperCase();
+		         tipoCliente = teclado.next().toUpperCase();
 		        if (tipoCliente == null) {
 		            System.out.println("Tipo de Cliente no válido. Por favor, ingrese un tipo de propiedad válido.");
 		        }
@@ -420,16 +427,16 @@ public class InterfazInmobiliaria {
 			
 
 			
-			if (tipoCliente == "INQUILINO") {
+			if (tipoCliente.equals( "INQUILINO")) {
 				 System.out.print("Ingrese el DNI del INQUILINO: ");
 			        Integer dniCliente = teclado.nextInt();
-			        teclado.nextLine();
+			        teclado.next();
 				
 				System.out.print("Ingrese el nombre del INQUILINO: ");
-		        String nombreCliente = teclado.nextLine();
+		        String nombreCliente = teclado.next();
 		        
 		        System.out.print("Ingrese el apellido del INQUILINO: ");
-		        String apellidoCliente = teclado.nextLine();
+		        String apellidoCliente = teclado.next();
 		        Inquilino nuevoCliente = new Inquilino(dniCliente, nombreCliente, apellidoCliente);
 		        Boolean agregado = inmobiliaria.addInquilino(nuevoCliente);	  
 			
@@ -444,16 +451,16 @@ public class InterfazInmobiliaria {
 			
 			
 			
-			if (tipoCliente == "PROPIETARIO") {
+			if (tipoCliente.equals( "PROPIETARIO")) {
 				 System.out.print("Ingrese el DNI del INQUILINO: ");
 			        Integer dniCliente = teclado.nextInt();
-			        teclado.nextLine();
+			        teclado.next();
 				
 				System.out.print("Ingrese el nombre del INQUILINO: ");
-		        String nombreCliente = teclado.nextLine();
+		        String nombreCliente = teclado.next();
 		        
 		        System.out.print("Ingrese el apellido del INQUILINO: ");
-		        String apellidoCliente = teclado.nextLine();
+		        String apellidoCliente = teclado.next();
 		        Inquilino nuevoCliente = new Inquilino(dniCliente, nombreCliente, apellidoCliente);
 		        Boolean agregado = inmobiliaria.addInquilino(nuevoCliente);	  
 			
@@ -466,25 +473,25 @@ public class InterfazInmobiliaria {
 		        }
 			}    
 						
-	        teclado.close();
+	        
 	    }
 		
 	
 	private static void mostrarPropiedadesPorPrecio(Inmobiliaria inmobiliaria) {
         
-		Scanner teclado = new Scanner(System.in);
+		
 		  
 	    String tipoPropiedad;
 	    do {
 	        System.out.print("Ingrese el tipo de propiedad (CASA, DEPTO, PH, TERRENO, CAMPO): \n");
-	        tipoPropiedad = teclado.nextLine().toUpperCase();
+	        tipoPropiedad = teclado.next().toUpperCase();
 	        if (tipoPropiedad == null) {
 	            System.out.println("Tipo de propiedad no válido. Por favor, ingrese un tipo de propiedad válido.");
 	        }
 	    } while (tipoPropiedad == null);
 
 	    
-		   if (tipoPropiedad == "CASA") {
+		   if (tipoPropiedad.equals("CASA") ) {
 		
 			   Casa[] casas = inmobiliaria.obtenerPropiedadesOrdenadasPorPrecioCasa();
 
@@ -495,7 +502,7 @@ public class InterfazInmobiliaria {
 			   }
 		   }
 		   
-		   if (tipoPropiedad == "DEPTO") {
+		   if (tipoPropiedad.equals("DEPTO") ) {
 				
 			   Departamento[] deptos = inmobiliaria.obtenerPropiedadesOrdenadasPorPrecioDepto();
 
@@ -505,24 +512,24 @@ public class InterfazInmobiliaria {
 				   System.out.println(departamento); 
 			   }
 		   }
-		   teclado.close();
+		   
 	}
 	
 	private static void mostrarPropiedadesPorUbicacion(Inmobiliaria inmobiliaria) {
 		 
-		Scanner teclado = new Scanner(System.in);
+		
 		  
 	    String tipoPropiedad;
 	    do {
 	        System.out.print("Ingrese el tipo de propiedad (CASA, DEPTO, PH, TERRENO, CAMPO): \n");
-	        tipoPropiedad = teclado.nextLine().toUpperCase();
+	        tipoPropiedad = teclado.next().toUpperCase();
 	        if (tipoPropiedad == null) {
 	            System.out.println("Tipo de propiedad no válido. Por favor, ingrese un tipo de propiedad válido.");
 	        }
 	    } while (tipoPropiedad == null);
 
 	    
-		   if (tipoPropiedad == "CASA") {
+		   if (tipoPropiedad.equals("CASA") ) {
 		
 		   Casa[] casas = inmobiliaria.obtenerPropiedadesOrdenadasPorUbicacionCasa();
 
@@ -533,7 +540,7 @@ public class InterfazInmobiliaria {
 	        }
 	   }   
 		   
-		   if (tipoPropiedad == "DEPTO") {
+		   if (tipoPropiedad.equals("DEPTO") ) {
 				
 			   Departamento[] deptos = inmobiliaria.obtenerPropiedadesOrdenadasPorPrecioDepto();
 
@@ -543,7 +550,7 @@ public class InterfazInmobiliaria {
 		            System.out.println(depto); 
 		        }
 		   } 
-		teclado.close();
+		
 	}
 			
 	
